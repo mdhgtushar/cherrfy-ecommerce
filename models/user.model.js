0c4747b82d0e7519ea45472287cc755e9f8e1f78
Hobayer Golondaz <81422400+mdhgtushar@users.noreply.github.com>
2024-12-02 14:19:05 +0000

    cnges

--- models/user.model.js
+++ models/user.model.js
@@ -26,6 +26,5 @@ const userSchema = new mongoose.Schema({
     resetPasswordExpiresAt: Date,
     verificationToken: String,
     verificationTokenExpiresAt: Date,
-}, {timestamps : true});
-
+}, { timestamps: true });
 export const User = mongoose.model("User", userSchema);
